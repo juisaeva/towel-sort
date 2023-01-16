7c2de05beb150eef9b7e7af9010db00b1d5233d6
Yulia Isaeva <pulinu@icloud.com>
2023-01-16 10:04:52 +0500

    init: add correct solution

--- src/index.js
+++ src/index.js
@@ -2,5 +2,37 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+    //   return [];
+
+    // if(matrix === null || matrix === undefined) 
+    if(!matrix) {
+        return [];
+      }
+    
+      const newArr = [];
+    
+      matrix.forEach(function (value, i) {
+        
+        i % 2 === 0 ? newArr.push(value) : newArr.push(value.reverse());
+      
+        // if(i % 2 === 0) {
+        //   newArr.push(value);
+        // } else {
+        //   newArr.push(value.reverse());
+        // }
+    
+      });
+    
+      return newArr.flat();
 }
+
+
+/*
+[
+ [ 1, 2, 3 ],
+ [ 4, 5, 6 ],
+ [ 7, 8, 9 ],
+]
+
+должно вернуться как [1, 2, 3, 6, 5, 4, 7, 8, 9 ] => сортировка по even(с reverse)/odd и записать в новый массив
+*/
